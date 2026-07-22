3c9d0f09836c4ac9e9e8f905e52132a500c3d34a
Tomás Schlotahuer <tomsschlotahuer@hotmail.com>
2026-07-22 17:11:39 -0300

    Set site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://0tomi.github.io',
 	// Enable Svelte to support Svelte components.
 	integrations: [svelte()],
 });
